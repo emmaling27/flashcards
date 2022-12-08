b124492e8679276f5572b3054fffd8300533d4c8
Emma Forman Ling <emma@convex.dev>
2022-12-08 11:30:42 -0800

    store user on app mount

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,23 @@
 import { FormEvent, useEffect, useState } from 'react'
+import { Id } from '../convex/_generated/dataModel'
 import { useMutation, useQuery } from '../convex/_generated/react'
 
 export default function App() {
+  const [_userId, setUserId] = useState<Id<'users'> | null>(null)
+  const storeUser = useMutation('storeUser')
+  // Call the `storeUser` mutation function to store
+  // the current user in the `users` table and return the `Id` value.
+  useEffect(() => {
+    // Store the user in the database.
+    // Recall that `storeUser` gets the user information via the `auth`
+    // object on the server. You don't need to pass anything manually here.
+    async function createUser() {
+      const id = await storeUser()
+      setUserId(id)
+    }
+    createUser().catch(console.error)
+    return () => setUserId(null)
+  }, [storeUser])
   const messages = useQuery('listMessages') || []
 
   const [newMessageText, setNewMessageText] = useState('')
