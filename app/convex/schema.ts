5d8ba1c102388528b57dccae331e7292a2458f29
Emma Forman Ling <emma@convex.dev>
2022-12-08 11:24:29 -0800

    add full schema

--- app/convex/schema.ts
+++ app/convex/schema.ts
@@ -13,5 +13,24 @@ export default defineSchema({
     name: s.string(),
     description: s.string(),
     creator: s.id("users")
+  }),
+  cards: defineTable({
+    front: s.string(),
+    back: s.string(),
+    creator: s.id("users")
+  }),
+  card_decks: defineTable({
+    deck: s.id("decks"),
+    card: s.id("cards"),
+  }),
+  cards_due: defineTable({
+    card: s.id("cards"),
+    user: s.id("users"),
+    due: s.number()
+  }),
+  history: defineTable({
+    user: s.id("users"),
+    card: s.id("cards"),
+    difficulty: s.string(),
   })
 })
