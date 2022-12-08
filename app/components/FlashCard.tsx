e18de1c1c35b76d113850d809444e6e944a37b39
Alex Cole <alex@convex.dev>
2022-12-08 14:46:06 -0800

    card buttons

--- app/components/FlashCard.tsx
+++ app/components/FlashCard.tsx
@@ -1,17 +1,40 @@
 import { useState } from 'react'
 import { Document } from '../convex/_generated/dataModel'
 import Image from 'next/image'
+import { useMutation } from '../convex/_generated/react'
 export default function ({ card }: { card: Document<'cards'> }) {
   const [front, setFront] = useState(true)
-
+  const reviewCard = useMutation('reviewCard')
   const flip = () => {
     setFront(!front)
   }
   return (
-    <div className="card" onClick={flip}>
-      <div className="card-inner" style={{ backgroundColor: card.color }}>
-        <div className="card-front">{card.front}</div>
-        <div className="card-back">{card.back}</div>
+    <div className="card-container">
+      <div className="card" onClick={flip}>
+        <div className="card-inner" style={{ backgroundColor: card.color }}>
+          <div className="card-front">{card.front}</div>
+          <div className="card-back">{card.back}</div>
+        </div>
+      </div>
+      <div className="review-card">
+        <button
+          onClick={() => reviewCard(card._id, 'Good')}
+          title="Mark card as correct"
+        >
+          👍
+        </button>
+        <button
+          onClick={() => reviewCard(card._id, 'Hard')}
+          title="Mark card as incorrect"
+        >
+          👎
+        </button>
+        <button
+          onClick={() => reviewCard(card._id, 'Easy')}
+          title="Don't show this card again for a while"
+        >
+          🕰
+        </button>
       </div>
     </div>
   )
