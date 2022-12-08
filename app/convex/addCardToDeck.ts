16746082baeee9eb4796b3a16cfcb581ce8b3ab4
Lee Danilek <lee@convex.dev>
2022-12-08 14:42:56 -0800

    avoid duplicate cards in deck

--- app/convex/addCardToDeck.ts
+++ app/convex/addCardToDeck.ts
@@ -5,6 +5,11 @@ import { getUser } from './addDeck'
 
 export default mutation(async ({ db, auth }, cardId: Id<'cards'>, deckId: Id<'decks'>) => {
   const user = await getUser(db, auth);
+  const currentCardDeck = await db.query('card_decks').withIndex('by_deck', q => q.eq('deck', deckId).eq('card', cardId)).first();
+  if (currentCardDeck !== null) {
+    console.log('card is already in deck. skipping');
+    return;
+  }
   await db.insert('card_decks', {
     deck: deckId,
     card: cardId,
