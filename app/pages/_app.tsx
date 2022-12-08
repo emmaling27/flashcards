27bcd09cf5937e5301625f9578158c9b8f12537b
Emma Forman Ling <emma@convex.dev>
2022-12-08 10:26:15 -0800

    use auth

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,15 +1,63 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import convexConfig from '../convex.json'
 
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithAuth0 } from 'convex/react-auth0'
 import clientConfig from '../convex/_generated/clientConfig'
+import { useAuth0 } from '@auth0/auth0-react'
 const convex = new ConvexReactClient(clientConfig)
+const authInfo = convexConfig.authInfo[0]
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ConvexProvider client={convex}>
+    <ConvexProviderWithAuth0
+      client={convex}
+      authInfo={authInfo}
+      loggedOut={<Login />}
+    >
+      <div className="text-center">
+        <span>
+          <Logout />
+        </span>
+      </div>
       <Component {...pageProps} />
-    </ConvexProvider>
+    </ConvexProviderWithAuth0>
+  )
+}
+
+export function Login() {
+  const { isLoading, loginWithRedirect } = useAuth0()
+  if (isLoading) {
+    return <button className="btn btn-primary">Loading...</button>
+  }
+  return (
+    <main className="py-4">
+      <h1 className="text-center">Convex Chat</h1>
+      <div className="text-center">
+        <span>
+          <button className="btn btn-primary" onClick={loginWithRedirect}>
+            Log in
+          </button>
+        </span>
+      </div>
+    </main>
+  )
+}
+
+function Logout() {
+  const { logout, user } = useAuth0()
+  return (
+    <div>
+      {/* We know this component only renders if the user is logged in. */}
+      <p>Logged in{user!.name ? ` as ${user!.name}` : ''}</p>
+      <button
+        className="btn btn-primary"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Log out
+      </button>
+    </div>
   )
 }
 
