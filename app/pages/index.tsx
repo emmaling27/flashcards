bdc7a4884decadb4f21842c2e97cd1727f505cad
Emma Forman Ling <emma@convex.dev>
2022-12-08 11:49:01 -0800

    replace messages with decks on the home page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,44 +2,57 @@ import { FormEvent, useEffect, useState } from 'react'
 import { useMutation, useQuery } from '../convex/_generated/react'
 
 export default function App() {
-  const messages = useQuery('listMessages') || []
-
-  const [newMessageText, setNewMessageText] = useState('')
-  const sendMessage = useMutation('sendMessage')
-
-  const [name, setName] = useState('user')
-
+  const [_userId, setUserId] = useState<Id<'users'> | null>(null)
+  const storeUser = useMutation('storeUser')
+  // Call the `storeUser` mutation function to store
+  // the current user in the `users` table and return the `Id` value.
   useEffect(() => {
-    setName('User ' + Math.floor(Math.random() * 10000))
-  }, [])
+    // Store the user in the database.
+    // Recall that `storeUser` gets the user information via the `auth`
+    // object on the server. You don't need to pass anything manually here.
+    async function createUser() {
+      const id = await storeUser()
+      setUserId(id)
+    }
+    createUser().catch(console.error)
+    return () => setUserId(null)
+  }, [storeUser])
+  const decks = useQuery('listDecks') || []
 
-  async function handleSendMessage(event: FormEvent) {
+  const [newDeckName, setNewDeckName] = useState('')
+  const [newDeckDescription, setNewDeckDescription] = useState('')
+  const addDeck = useMutation('addDeck')
+
+  async function handleAddDeck(event: FormEvent) {
     event.preventDefault()
-    setNewMessageText('')
-    await sendMessage(newMessageText, name)
+    setNewDeckName('')
+    setNewDeckDescription('')
+    await addDeck(newDeckName, newDeckDescription)
   }
   return (
     <main>
       <h1>Flashcards</h1>
-      <p className="badge">
-        <span>{name}</span>
-      </p>
       <ul>
-        {messages.map((message) => (
-          <li key={message._id.toString()}>
-            <span>{message.author}:</span>
-            <span>{message.body}</span>
-            <span>{new Date(message._creationTime).toLocaleTimeString()}</span>
+        {decks.map((deck) => (
+          <li key={deck._id.toString()}>
+            <span>{deck.name}</span>
+            <span>{deck.description}</span>
+            <span>{new Date(deck._creationTime).toLocaleTimeString()}</span>
           </li>
         ))}
       </ul>
-      <form onSubmit={handleSendMessage}>
+      <form onSubmit={handleAddDeck}>
         <input
-          value={newMessageText}
-          onChange={(event) => setNewMessageText(event.target.value)}
-          placeholder="Write a message…"
+          value={newDeckName}
+          onChange={(event) => setNewDeckName(event.target.value)}
+          placeholder="Name"
         />
-        <input type="submit" value="Send" disabled={!newMessageText} />
+        <input
+          value={newDeckDescription}
+          onChange={(event) => setNewDeckDescription(event.target.value)}
+          placeholder="Description"
+        />
+        <input type="submit" value="Add deck" disabled={!newDeckName} />
       </form>
     </main>
   )
